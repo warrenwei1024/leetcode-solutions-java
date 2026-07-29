d0dc96c10144f22bcb0a0512d48041316f06b3df
weijunjie <warrenwei1024@gmail.com>
2026-07-30 00:40:50 +0800

    leetcode 0075 颜色分类 normal

--- src/main/java/com/leetcode/top100liked/lc_0075_sort_colors/Solution.java
+++ src/main/java/com/leetcode/top100liked/lc_0075_sort_colors/Solution.java
@@ -1,5 +1,16 @@
 package com.leetcode.top100liked.lc_0075_sort_colors;
 
+/**
+ * LeetCode 75. 颜色分类
+ * <p>
+ * 给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums，
+ * 原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+ * 使用整数 0、1 和 2 分别表示红色、白色和蓝色。必须不使用库函数排序。
+ * <p>
+ * 
+ * @author weijunjie
+ * @date 2026-07-09
+ */
 public class Solution {
     public void sortColors(int[] nums) {
         if (nums == null || nums.length == 0) {
@@ -35,3 +46,10 @@ public class Solution {
         nums[j] = temp;
     }
 }
+
+// 核心思路
+// 荷兰国旗问题：三指针，left 指向下一个 0 的位置，right 指向下一个 2 的位置，
+// curr 遍历数组。遇到 0 换到左边，遇到 2 换到右边，遇到 1 跳过。
+//
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
