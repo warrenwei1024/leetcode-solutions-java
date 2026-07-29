066dcaf6276467968da8180306879aee2fe13143
weijunjie <warrenwei1024@gmail.com>
2026-07-30 00:47:53 +0800

    leetcode 0560 和为K的子数组 normal

--- src/main/java/com/leetcode/top100liked/lc_0560_subarray_sum_equals_k/Solution.java
+++ src/main/java/com/leetcode/top100liked/lc_0560_subarray_sum_equals_k/Solution.java
@@ -3,6 +3,15 @@ package com.leetcode.top100liked.lc_0560_subarray_sum_equals_k;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * LeetCode 560. 和为 K 的子数组
+ * <p>
+ * 给你一个整数数组 nums 和一个整数 k，请你统计并返回该数组中和为 k 的连续子数组的个数。
+ * <p>
+ * 
+ * @author weijunjie
+ * @date 2026-07-09
+ */
 public class Solution {
     /**
      * 给定一个整数数组 nums 和一个整数 k，
@@ -52,3 +61,10 @@ public class Solution {
         System.out.println(solution.subarraySum(new int[]{1, 2, -1, 1, 2}, 3));       // 输出: 3
     }
 }
+
+// 核心思路
+// 前缀和 + 哈希表：prefixSum[j] - prefixSum[i-1] = k 等价于 prefixSum[i-1] = prefixSum[j] - k。
+// 遍历时用哈希表记录每个前缀和出现的次数，每次查 currentSum - k 在哈希表中的次数累加。
+//
+// 时间复杂度: O(n)
+// 空间复杂度: O(n)
