603061bb3fe2617a27b7474b8ca50744d346ccad
weijunjie <warrenwei1024@gmail.com>
2026-07-30 00:47:52 +0800

    leetcode 0279 完全平方数 normal

--- src/main/java/com/leetcode/top100liked/lc_0279_perfect_squares/Solution.java
+++ src/main/java/com/leetcode/top100liked/lc_0279_perfect_squares/Solution.java
@@ -1,5 +1,15 @@
 package com.leetcode.top100liked.lc_0279_perfect_squares;
 
+/**
+ * LeetCode 279. 完全平方数
+ * <p>
+ * 给你一个整数 n，返回和为 n 的完全平方数的最少数量。
+ * 完全平方数是一个整数，其值等于另一个整数的平方；可重复使用。
+ * <p>
+ * 
+ * @author weijunjie
+ * @date 2026-07-09
+ */
 public class Solution {
     public int numSquares(int n) {
         // dp[i] 表示凑齐整数 i 所需的最少完全平方数个数
@@ -22,3 +32,10 @@ public class Solution {
         return dp[n];
     }
 }
+
+// 核心思路
+// 动态规划：dp[i] 表示凑齐整数 i 所需的最少完全平方数个数。
+// 状态转移：dp[i] = min(dp[i - j*j] + 1)，枚举所有 j 满足 j*j <= i。
+//
+// 时间复杂度: O(n * sqrt(n))
+// 空间复杂度: O(n)
