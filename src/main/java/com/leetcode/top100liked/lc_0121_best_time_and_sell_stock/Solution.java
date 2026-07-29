24ca3ae11e7467d5faaef85ab40f4ec3b247cc9a
weijunjie <warrenwei1024@gmail.com>
2026-07-30 00:43:30 +0800

    leetcode 0121 买卖股票的最佳时机 easy

--- src/main/java/com/leetcode/top100liked/lc_0121_best_time_and_sell_stock/Solution.java
+++ src/main/java/com/leetcode/top100liked/lc_0121_best_time_and_sell_stock/Solution.java
@@ -1,5 +1,16 @@
 package com.leetcode.top100liked.lc_0121_best_time_and_sell_stock;
 
+/**
+ * LeetCode 121. 买卖股票的最佳时机
+ * <p>
+ * 给定一个数组 prices，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ * 你只能选择某一天买入这只股票，并选择在未来的某一个不同的日子卖出该股票。
+ * 设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 
+ * @author weijunjie
+ * @date 2026-07-09
+ */
 public class Solution {
     public int maxProfit(int[] prices) {
         if(prices == null || prices.length == 0) return 0;
@@ -15,3 +26,10 @@ public class Solution {
         return maxProfit;
     }
 }
+
+// 核心思路
+// 遍历过程中维护历史最低价格 minPrice，每天计算当日卖出可得利润，更新最大利润。
+// 本质是动态规划：第 i 天卖出的最大利润 = prices[i] - min(prices[0..i-1])。
+//
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
