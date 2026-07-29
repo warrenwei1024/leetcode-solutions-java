230c7b5d48f1c3f312fd58795a2b62b5ff5b5a98
weijunjie <warrenwei1024@gmail.com>
2026-07-30 00:45:53 +0800

    leetcode 0160 相交链表 easy

--- src/main/java/com/leetcode/top100liked/lc_0160_intersection_of_two_linked_lists/Solution.java
+++ src/main/java/com/leetcode/top100liked/lc_0160_intersection_of_two_linked_lists/Solution.java
@@ -1,5 +1,16 @@
 package com.leetcode.top100liked.lc_0160_intersection_of_two_linked_lists;
 
+/**
+ * LeetCode 160. 相交链表
+ * <p>
+ * 给你两个单链表的头节点 headA 和 headB，请你找出并返回两个单链表相交的起始节点。
+ * 如果两个链表不存在相交节点，返回 null。
+ * <p>
+ * 
+ * @author weijunjie
+ * @date 2026-07-09
+ */
+
 
 class ListNode {
     int val;
@@ -35,8 +46,15 @@ public class Solution {
         return pA;
     }
 }
+
+// 核心思路
+// 双指针遍历：pA 遍历完 A 后指向 B，pB 遍历完 B 后指向 A。
+// 若相交，两指针会在交点相遇（走过的总路程相同）；若不相交，同时为 null。
 //
-//public class Solution {
+// 时间复杂度: O(m + n)
+// 空间复杂度: O(1)
+//
+// 方法二：计算长度差值，对齐后同步遍历
 //    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 //        if (headA == null || headB == null) return null;
 //
