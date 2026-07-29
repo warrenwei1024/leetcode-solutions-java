aa166045d19dee43bd4d0a507faee502ab94dd42
weijunjie <warrenwei1024@gmail.com>
2026-07-30 00:45:53 +0800

    leetcode 0189 轮转数组 normal

--- src/main/java/com/leetcode/top100liked/lc_0189_rotate_array/Solution.java
+++ src/main/java/com/leetcode/top100liked/lc_0189_rotate_array/Solution.java
@@ -1,5 +1,14 @@
 package com.leetcode.top100liked.lc_0189_rotate_array;
 
+/**
+ * LeetCode 189. 轮转数组
+ * <p>
+ * 给定一个整数数组 nums，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
+ * <p>
+ * 
+ * @author weijunjie
+ * @date 2026-07-09
+ */
 public class Solution {
 
     public void rotate(int[] nums, int k) {
@@ -34,3 +43,10 @@ public class Solution {
         }
     }
 }
+
+// 核心思路
+// 三次翻转：先翻转整个数组，再翻转前 k 个元素，最后翻转剩余元素。
+// 注意先对 k 取模处理 k > n 的情况。
+//
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
