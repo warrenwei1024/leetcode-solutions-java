edb1e23f7f55608af6e4deec8850efaa91de6a95
weijunjie <warrenwei1024@gmail.com>
2026-07-30 00:45:53 +0800

    leetcode 0206 反转链表 easy

--- src/main/java/com/leetcode/top100liked/lc_0206_reverse_linked_list/Solution.java
+++ src/main/java/com/leetcode/top100liked/lc_0206_reverse_linked_list/Solution.java
@@ -1,5 +1,14 @@
 package com.leetcode.top100liked.lc_0206_reverse_linked_list;
 
+/**
+ * LeetCode 206. 反转链表
+ * <p>
+ * 给你单链表的头节点 head，请你反转链表，并返回反转后的链表。
+ * <p>
+ * 
+ * @author weijunjie
+ * @date 2026-07-09
+ */
 
 
 class ListNode {
@@ -37,3 +46,10 @@ public class Solution {
 
     }
 }
+
+// 核心思路
+// 迭代法：用 prev、curr、next 三个指针遍历链表，逐个反转节点的指向。
+// curr.next = prev 后，三指针同步前移。
+//
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
